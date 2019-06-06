b427076ad7a7dacbdcb13dca7e9bea992d05db81
Vasilii Fadeev <vfadeev89@mail.ru>
2019-06-06 13:44:39 +0300

    Добавлен eslint и конфигурация airbnb

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,31 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ['@babel/polyfill', './src/js/index.js'],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js'
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 8080
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: "./src/index.html"
-        })
+  entry: ['@babel/polyfill', './src/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    }
+  },
 };
